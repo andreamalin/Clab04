ea6ce671e9aa237213944ea2de341636d97f46ab
Brandon <brandonhernandez_10@outlook.es>
2021-08-15 16:04:10 -0600

    Hola

--- Clab03.py
+++ Clab03.py
@@ -5,33 +5,15 @@ import random
 class clab03():
 
     def __init__(self):
-        self.img = self.img2bits('p1.jpg')
-        img = self.img
-        self.b1 = self.xor(img, self.wichmanhill(len(img)))
-        self.b2 = self.xor(img, self.lfsr(len(img), '11001001', (8,7,6,1)))
-        self.b3 = self.xor(img, self.LCG(612,5115,7212))
-        self.m1 = self.xor(img, self.LCG(6,55,11))
-        self.m2 = self.xor(img, self.LCG(6,55,37))
-        self.m3 = self.xor(img, self.LCG(6,55,150))
+        self.length = 1000000
+        self.b1 = self.wichmanhill(1000000)
+        self.b2 = self.lfsr(1000000, '11001001', (8, 7, 6, 1))
+        self.b3 = self.LCG(612, 5115, 7212)
+        self.m1 = self.LCG(6, 55, 11)
+        self.m2 = self.LCG(6, 55, 37)
+        self.m3 = self.LCG(6, 55, 150)
         self.good = [self.b1, self.b2, self.b3]
         self.bad = [self.m1, self.m2, self.m3]
-            
-    def img2bits(self,imgPath):
-
-        img = cv2.imread(imgPath, 2) # Abrir imagen
-        ret, bw = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY) # Convertir a binario
-        # Para ver la imagen en blanco y negro
-        cadena = ''
-        for row in bw:
-            for i in range(len(row)):
-                if (row[i] == 255):
-                    cadena += '0'
-                else:
-                    cadena += '1'
-            # Para ver la imagen al hacer zoom out :D
-            # cadena += '\n'
-            
-        return cadena
 
     def lfsr(self,lonFinal, seed, taps):
         # Resultado y sr con la semilla inicial
--- simulation.py
+++ simulation.py
@@ -17,26 +17,69 @@ def main():
     tests = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0}
     prgs = clab03()
     testRunner = TestOverview()
+    n = 1000
+
+    # for _ in range(n):
+    #   testResult = testRunner.runTest(prgs.wichmanhill(10000))
+    #   failedTests = 10 - sum(list(testResult['Conclusion']))
+
+    #   tests[failedTests] += 1
+    # showHistogram(tests)
+    # print('primero')
+    # for i in range(1, 11):
+    #   print(i, tests[i]/n)
+    # tests = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0}
+
+    # for _ in range(n):
+    #   testResult = testRunner.runTest(
+    #       prgs.lfsr(10000, '11001001', (8, 7, 6, 1)))
+    #   failedTests = 10 - sum(list(testResult['Conclusion']))
+
+    #   tests[failedTests] += 1
+    # showHistogram(tests)
+    # print('segundo')
+    # for i in range(1, 11):
+    #   print(tests[i]/n)
+    # tests = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0}
+
+    for _ in range(n):
+        testResult = testRunner.runTest(
+            prgs.LCG(612, 5115, 7212))
+        failedTests = 10 - sum(list(testResult['Conclusion']))
+
+        tests[failedTests] += 1
+    print('tercero')
+    for i in range(1, 11):
+        print(tests[i]/n)
+    showHistogram(tests)
+
+
+def main2():
+    # Se obtienen las variables que se utilizaran
+    tests = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0}
+    prgs = clab03()
+    testRunner = TestOverview()
 
     for _ in range(350):
-      testResult = testRunner.runTest(prgs.wichmanhill(10000))
-      failedTests = 10 - sum(list(testResult['Conclusion']))
+        testResult = testRunner.runTest(
+            prgs.LCG(6, 55, 11))
+        failedTests = 10 - sum(list(testResult['Conclusion']))
 
-      tests[failedTests] += 1
-
-    for _ in range(350):
-      testResult = testRunner.runTest(
-          prgs.lfsr(10000, '11001001', (8, 7, 6, 1)))
-      failedTests = 10 - sum(list(testResult['Conclusion']))
-
-      tests[failedTests] += 1
+        tests[failedTests] += 1
 
     for _ in range(350):
-      testResult = testRunner.runTest(
-          prgs.LCG(612, 5115, 7212))
-      failedTests = 10 - sum(list(testResult['Conclusion']))
+        testResult = testRunner.runTest(
+            prgs.LCG(6, 55, 37))
+        failedTests = 10 - sum(list(testResult['Conclusion']))
 
-      tests[failedTests] += 1
+        tests[failedTests] += 1
+
+    for _ in range(350):
+        testResult = testRunner.runTest(
+            prgs.LCG(6, 55, 150))
+        failedTests = 10 - sum(list(testResult['Conclusion']))
+
+        tests[failedTests] += 1
 
     showHistogram(tests)
 
